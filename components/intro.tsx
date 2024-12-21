ff9dd9b01d505147dbbdeaf2b347b847053edc51
Enes Samed <enessamed422@gmail.com>
2024-12-21 22:50:25 +0300

    changed favicon

--- components/intro.tsx
+++ components/intro.tsx
@@ -20,7 +20,7 @@ export default function Intro() {
         <div className='relative'>
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1, scale: 1.2 }}
             transition={{
               type: 'tween',
               duration: 0.2,
@@ -33,14 +33,14 @@ export default function Intro() {
               height='192'
               quality='95'
               priority={true}
-              className='h-24 w-24 rounded-full object-cover object-left-bottom border-[0.35rem] border-white shadow-xl'
+              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
             />
           </motion.div>
 
           <motion.span
             className='absolute bottom-0 right-0 text-4xl'
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, scale: 0,bottom:100 }}
+            animate={{ opacity: 1, scale: 1, bottom:-16 }}
             transition={{
               type: 'spring',
               stiffness: 125,
