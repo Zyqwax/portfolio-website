be4e54c821ea884743b07144a18514153df7eefc
Enes Samed <enessamed422@gmail.com>
2024-12-25 22:48:19 +0300

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
         <link rel='icon' type='image/png' sizes='32x32' href='/icon/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='96x96' href='/icon/favicon-96x96.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/icon/favicon-16x16.png' />
+        <link rel='icon' href='/favicon.ico' />
         <link rel='manifest' href='/icon/manifest.json' />
         <meta name='msapplication-TileColor' content='#ffffff' />
         <meta name='msapplication-TileImage' content='/icon/ms-icon-144x144.png' />
@@ -43,8 +44,8 @@ export default function RootLayout({ children }) {
         <meta name='author' content='Enes Samed Gözlü' />
         <meta property='og:title' content='Enes Samed Gözlü - Portföy' />
         <meta property='og:description' content="Yazılım öğrencisi Enes Samed Gözlü'nün portföy sitesi. Yaratıcı projeler, yazılım geliştirme deneyimleri ve teknoloji hakkındaki bilgiler." />
-        <meta property='og:image' content='/thumbnail.png'/>
-        <meta property='og:url' content='https://enessamedgozlu.me'/>
+        <meta property='og:image' content='/thumbnail.png' />
+        <meta property='og:url' content='https://enessamedgozlu.me' />
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
