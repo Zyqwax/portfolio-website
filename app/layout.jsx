ca6f09a5d123ad7a84ef2e529fd3f896c2729042
Enes Samed <enessamed422@gmail.com>
2024-12-28 20:56:10 +0300

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
         <meta property='og:image' content='/thumbnail.png' />
         <meta property='og:url' content='https://enessamedgozlu.me/' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
       </head>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
